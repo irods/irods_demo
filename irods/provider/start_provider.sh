7248de9c3fbbb2580dd782af1b87265878f5cca3
bh9 <bh9@sanger.ac.uk>
2020-06-12 15:54:37 +0000

    when we restart the provider, we need to not rerun the setup_irods script
    clean up a couple debug commands and correct the other start_provider script

--- irods/provider/start_provider.sh
+++ irods/provider/start_provider.sh
@@ -11,9 +11,13 @@ done
 echo Postgres took approximately $counter seconds to fully start ...
 
 # Set up iRODS.
+if [ ! -e /var/lib/irods/VERSION.json ]; then
 python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input
+pkill irodsServer
+fi
+sudo -iu irods bash -c "cd /usr/sbin; ./irodsServer -u"
 
 # Keep container running if the test fails.
-tail -f /dev/null
+#tail -f /dev/null
 # Is this better? sleep 2147483647d
 
