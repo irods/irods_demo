446dd5b98a0d7ede0eb252fb2cbfe46f72e0ecf7
Terrell Russell <terrellrussell@gmail.com>
2025-07-17 07:44:59 -0400

    no more double start, just had to wait for the server to let go

--- irods_catalog_provider/firstrun.sh
+++ irods_catalog_provider/firstrun.sh
@@ -11,5 +11,8 @@ done
 # create s3 resource
 su - irods -c 'iadmin mkresc s3resc s3 irods-catalog-provider:/demobucket/thevault "S3_DEFAULT_HOSTNAME=minio:19000;S3_AUTH_FILE=/var/lib/irods/s3.keypair;S3_REGIONNAME=us-east-1;S3_RETRY_COUNT=1;S3_WAIT_TIME_SECONDS=3;S3_PROTO=HTTP;ARCHIVE_NAMING_POLICY=consistent;HOST_MODE=cacheless_attached"'
 
-# wait for any other services to see this service is healthy
-sleep 2
+# kill server and wait for it to stop
+kill $(cat /var/run/irods/irods-server.pid)
+while ps -F -p $(cat /var/run/irods/irods-server.pid) ; do
+    sleep 1
+done
