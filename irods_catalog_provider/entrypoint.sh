c19c8fd7245e3907ee2340af45838d598226f8ac
Terrell Russell <terrellrussell@gmail.com>
2025-07-16 20:54:16 -0400

    provider 501 is working, but with a double start

--- irods_catalog_provider/entrypoint.sh
+++ irods_catalog_provider/entrypoint.sh
@@ -15,14 +15,25 @@ unattended_install_file=/unattended_install.json
 if [ -e "${unattended_install_file}" ]; then
     echo "Running iRODS setup"
     sed -i "s/THE_HOSTNAME/${HOSTNAME}/g" ${unattended_install_file}
+
+    # unattended workaround for 5.0.1
+    sed -i '/^\s*setup_server_host(irods_config)/s/^/    /' /var/lib/irods/scripts/setup_irods.py
+    sed -i '/^\s*setup_server_host(irods_config)/{h;d;};/^\s*json_configuration_dict = None/{G;}' /var/lib/irods/scripts/setup_irods.py
+
     python3 /var/lib/irods/scripts/setup_irods.py --json_configuration_file ${unattended_install_file}
     rm ${unattended_install_file}
 
     echo "Running firstrun.sh"
     bash firstrun.sh
+#    echo "first run complete"
+#    ps aux
+#    ps -F -p $(cat /var/run/irods/irods-server.pid)
+#    echo "end of entrypoint if"
+else
+    echo "Starting server"
+    cd /usr/sbin
+    su irods -c 'irodsServer --stdout'
 fi
 
-echo "Starting server"
+#echo "Starting server"
 
-cd /usr/sbin
-su irods -c 'bash -c "./irodsServer -u"'
--- irods_catalog_provider/firstrun.sh
+++ irods_catalog_provider/firstrun.sh
@@ -1,8 +1,29 @@
-#!/bin/bash
+#!/bin/bash -x
 
-su - irods -c '/var/lib/irods/irodsctl start'
+su - irods -c 'irodsServer -d'
+
+# wait for server to respond
+CMD='echo -e "\x00\x00\x00\x33<MsgHeader_PI><type>HEARTBEAT</type></MsgHeader_PI>" | (exec 3<>/dev/tcp/127.0.0.1/1247; cat >&3; cat <&3; exec 3<&-)'
+i=1
+#echo "attempt ${i}"
+STATUS=$(eval ${CMD})
+#echo "STATUS = ${STATUS}"
+while [ "${STATUS}" != "HEARTBEAT" -a ${i} -le 10 ] ; do
+  sleep 1
+  i=$((i + 1))
+#  echo "attempt ${i}"
+  STATUS=$(eval ${CMD})
+#  echo "STATUS = ${STATUS}"
+done
 
 # create s3 resource
 su - irods -c 'iadmin mkresc s3resc s3 irods-catalog-provider:/demobucket/thevault "S3_DEFAULT_HOSTNAME=minio:19000;S3_AUTH_FILE=/var/lib/irods/s3.keypair;S3_REGIONNAME=us-east-1;S3_RETRY_COUNT=1;S3_WAIT_TIME_SECONDS=3;S3_PROTO=HTTP;ARCHIVE_NAMING_POLICY=consistent;HOST_MODE=cacheless_attached"'
 
-su - irods -c '/var/lib/irods/irodsctl stop'
+#cat /var/run/irods/irods-server.pid
+#ps -p $(cat /var/run/irods/irods-server.pid)
+#kill -TERM $(cat /var/run/irods/irods-server.pid)
+#echo $?
+#cat /var/run/irods/irods-server.pid
+#ps -p $(cat /var/run/irods/irods-server.pid)
+
+#echo "last line of first run"
