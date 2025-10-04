02a723bd5133d97b8bba435dc2d69a6c6df662c4
Terrell Russell <terrellrussell@gmail.com>
2025-10-03 22:10:07 -0400

    [#115] update provider and consumer to v5.0.2

--- irods_catalog_provider/entrypoint.sh
+++ irods_catalog_provider/entrypoint.sh
@@ -16,10 +16,6 @@ if [ -e "${unattended_install_file}" ]; then
     echo "Running iRODS setup"
     sed -i "s/THE_HOSTNAME/${HOSTNAME}/g" ${unattended_install_file}
 
-    # unattended workaround for 5.0.1
-    sed -i '/^\s*setup_server_host(irods_config)/s/^/    /' /var/lib/irods/scripts/setup_irods.py
-    sed -i '/^\s*setup_server_host(irods_config)/{h;d;};/^\s*json_configuration_dict = None/{G;}' /var/lib/irods/scripts/setup_irods.py
-
     python3 /var/lib/irods/scripts/setup_irods.py --json_configuration_file ${unattended_install_file}
     rm ${unattended_install_file}
 
